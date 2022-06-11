bcaa02510fbc671a86bdabb912695f8ecee46c50
mann0777 <mannamloh77@gmail.com>
2022-06-11 07:11:13 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import ExpenseItem from "./component/ExpenseItem";
+
+
+
+function App(){
+
+  const expenses = [
+
+    {id: 'e1', title: 'Car Insurance', amount: 294, date: new Date(2022, 6, 4)},
+    {id: 'e2', title: 'GOOgle', amount:5234, date: new Date(2022, 6, 4)},
+    {id: 'e3', title: 'facebook', amount: 194, date: new Date(2022, 6, 4)},
+    {id: 'e4', title: 'Yahoo', amount: 794, date: new Date(2022, 7, 4)},
+
+  ];
+
+
+  return(
+    <>
+    <div>
+    <h2> Lets get started</h2>
+
+    <div className="expenses">
+    <ExpenseItem 
+    title = {expenses[0].title} 
+    amount= {expenses[0].amount} 
+    date= {expenses[0].date}
+    ></ExpenseItem>
+
+    <ExpenseItem 
+    title = {expenses[1].title} 
+    amount= {expenses[1].amount} 
+    date= {expenses[1].date}
+    ></ExpenseItem>
+
+    <ExpenseItem title = 
+    {expenses[2].title} 
+    amount= {expenses[2].amount} 
+    date= {expenses[2].date}
+    ></ExpenseItem>
+
+    <ExpenseItem title = 
+    {expenses[3].title} 
+    amount= {expenses[3].amount} 
+    date= {expenses[3].date}
+    ></ExpenseItem>
+
     </div>
+    
+    </div>
+    
+</>
+
   );
+
+
 }
 
 export default App;
---
+++ src/component/Card.js
@@ -0,0 +1,10 @@
+import './Card.css';
+
+function Card(props){
+    const classes = 'card' + props.children;
+    return
+        <div className={classes}>{props.children}</div>
+    
+}
+
+export default Card;
