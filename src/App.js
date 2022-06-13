ca7816cb3ac470c260b3c24fbabb839187f37129
mann0777 <mannamloh77@gmail.com>
2022-06-13 11:05:15 +0530

    Use State and CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
+import Expenses from "./component/Expenses/Expenses";
 
-import ExpenseItem from "./component/ExpenseItem";
+
 
 
 
@@ -17,40 +18,12 @@ function App(){
 
 
   return(
-    <>
+  
     <div>
     <h2> Lets get started</h2>
-
-    <div className="expenses">
-    <ExpenseItem 
-    title = {expenses[0].title} 
-    amount= {expenses[0].amount} 
-    date= {expenses[0].date}
-    ></ExpenseItem>
-
-    <ExpenseItem 
-    title = {expenses[1].title} 
-    amount= {expenses[1].amount} 
-    date= {expenses[1].date}
-    ></ExpenseItem>
-
-    <ExpenseItem title = 
-    {expenses[2].title} 
-    amount= {expenses[2].amount} 
-    date= {expenses[2].date}
-    ></ExpenseItem>
-
-    <ExpenseItem title = 
-    {expenses[3].title} 
-    amount= {expenses[3].amount} 
-    date= {expenses[3].date}
-    ></ExpenseItem>
-
+    <Expenses items = {expenses} />
     </div>
     
-    </div>
-    
-</>
 
   );
 
