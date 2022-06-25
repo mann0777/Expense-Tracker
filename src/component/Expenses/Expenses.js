20bdfd86f7b3faadcf027b789321a519702febc0
mann0777 <mannamloh77@gmail.com>
2022-06-25 09:57:09 +0530

    using-stateful-lists-and-keys

--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -22,16 +22,13 @@ function Expenses(props) {
 
     {props.items.map((expense)=>(
     <ExpenseItem
+    key={expense.id}
     title={expense.title}
     amount={expense.amount}
     date={expense.date}
     />
     ))}
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
+      
       
     </Card>
   );
